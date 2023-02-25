0567f32682f8d570918402612627dee805197eae
Mephiresu <67890890+Mephiresu@users.noreply.github.com>
2023-02-25 19:29:27 +0500

    feat/auth: add User entity

---
+++ src/entities/user.ts
@@ -0,0 +1,38 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm'
+
+@Entity()
+export class User {
+  constructor(data: Partial<User>) {
+    Object.assign(this, data)
+  }
+
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  username: string
+  @Column()
+  password: string
+
+  @Column({ default: true })
+  requiredPasswordChange: boolean
+
+  @Column()
+  fullName: string
+  @Column()
+  email: string
+
+  @CreateDateColumn()
+  createdAt: Date
+  @UpdateDateColumn()
+  updatedAt: Date
+  @DeleteDateColumn()
+  deletedAt?: Date
+}
