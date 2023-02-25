3a9aa58879f7c89dd044185a0f07be88048fdefc
Mephiresu <67890890+Mephiresu@users.noreply.github.com>
2023-02-25 17:34:38 +0500

    fix: logger api

--- src/core/logger/logger.abstract.ts
+++ src/core/logger/logger.abstract.ts
@@ -1,6 +1,6 @@
 export abstract class Logger {
-  abstract debug(message: string, context?: object): void
-  abstract info(message: string, context?: object): void
-  abstract warn(message: string, context?: object): void
-  abstract error(message: string, context?: object)
+  abstract debug(message: string, context?: any): void
+  abstract info(message: string, context?: any): void
+  abstract warn(message: string, context?: any): void
+  abstract error(message: string, context?: any)
 }
