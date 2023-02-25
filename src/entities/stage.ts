020392cb8913c394df0dd94d1092cf033fce8c06
Mephiresu <67890890+Mephiresu@users.noreply.github.com>
2023-02-25 20:09:07 +0500

    feat/basic-services: feat add date columns

--- src/entities/stage.ts
+++ src/entities/stage.ts
@@ -1,4 +1,12 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
 import { Task } from './task'
 
 @Entity()
@@ -14,4 +22,15 @@ export class Stage {
 
   @OneToMany(() => Task, (tasks) => tasks.stage)
   tasks: Task
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  @DeleteDateColumn({
+    nullable: true,
+  })
+  deletedAt?: Date
 }
